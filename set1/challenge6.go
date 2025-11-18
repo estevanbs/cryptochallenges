c30ed0cbe5700167faadb88acb1ecd119501e410
Estevan <estevanbartmann@gmail.com>
2025-11-18 19:26:08 +0000

    calculate blocksToCheck using maxKeySize

--- set1/challenge6.go
+++ set1/challenge6.go
@@ -20,38 +20,36 @@ func hammingDistance(bytes1, bytes2 []byte) int {
 	return distance
 }
 
-func estimateKeySize(input []byte) int {
-	const MIN_KEY_SIZE = 2
-	const BLOCKS_TO_CHECK = 4
-	// use this MAX_KEY_SIZE to calculate hamming distance of all blocks
-	MAX_KEY_SIZE := len(input)/2 - BLOCKS_TO_CHECK - 1
+func estimateKeySize(input []byte, minKeySize, maxKeySize int) int {
+	// use this blocksToCheck to calculate hamming distance of all blocks
+	blocksToCheck := len(input)/2 - maxKeySize - 1
 
-	keySizeChecks := MAX_KEY_SIZE - MIN_KEY_SIZE
+	keySizeChecks := maxKeySize - minKeySize
 
-	results := make([][]float64, BLOCKS_TO_CHECK)
-	for i := 0; i < BLOCKS_TO_CHECK; i++ {
+	results := make([][]float64, blocksToCheck)
+	for i := 0; i < blocksToCheck; i++ {
 		results[i] = make([]float64, keySizeChecks)
-		for keySize := MIN_KEY_SIZE; keySize < MAX_KEY_SIZE; keySize++ {
+		for keySize := minKeySize; keySize < maxKeySize; keySize++ {
 			firstBytes := input[i : keySize+i]
 			secondBytes := input[keySize+i : (keySize+i)*2]
 
 			hd := hammingDistance(firstBytes, secondBytes)
 			nhd := float64(hd) / float64(keySize)
 
-			results[i][keySize-MIN_KEY_SIZE] = nhd
+			results[i][keySize-minKeySize] = nhd
 		}
 	}
 	smallestAverage := math.MaxFloat64
 	probableKeySize := 0
 	for i := 0; i < keySizeChecks; i++ {
 		var resultsSum float64 = 0
-		for j := 0; j < BLOCKS_TO_CHECK; j++ {
+		for j := 0; j < blocksToCheck; j++ {
 			resultsSum += results[j][i]
 		}
-		average := resultsSum / BLOCKS_TO_CHECK
+		average := resultsSum / float64(blocksToCheck)
 		if average < smallestAverage {
 			smallestAverage = average
-			probableKeySize = i + MIN_KEY_SIZE
+			probableKeySize = i + minKeySize
 		}
 	}
 	return probableKeySize
--- set1/challenge6_test.go
+++ set1/challenge6_test.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/hex"
+	"os"
 	"testing"
 )
 
 func TestEstimateKeySize(t *testing.T) {
 	text := "hey, this is a test. It should pass. My name isn't Jorginho!!"
-	key := "secret"
+	key := "secrete"
 
 	encryptedHex := encryptRepeatingKeyXor(text, key)
 
@@ -19,12 +21,36 @@ func TestEstimateKeySize(t *testing.T) {
 
 	test := len(key)
 
-	result := estimateKeySize(encrypted)
+	result := estimateKeySize(encrypted, 2, 10)
 
 	if result != test {
 		t.Fatalf(
-			"Estimating hamming distance for string \"%s\" failed. expected=%d, got=%d",
+			"Estimating key size for string \"%s\" failed. expected=%d, got=%d",
 			text,
+			test,
+			result)
+	}
+}
+
+
+func TestEstimateKeySizeFile(t *testing.T) {
+	content, e := os.ReadFile("challenge6_data.txt")
+	if e != nil {
+		panic(e)
+	}
+
+	encrypted, e := base64.StdEncoding.DecodeString(string(content))
+	if e != nil {
+		panic(e)
+	}
+
+	test := 29 // not sure
+
+	result := estimateKeySize(encrypted, 2, 40)
+
+	if result != test {
+		t.Fatalf(
+			"Estimating key size failed. expected=%d, got=%d",
 			test,
 			result)
 	}
